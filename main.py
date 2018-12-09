e205e5f7e4116b90679ea0ed23f3abfde0fec452
logan <logan.orr@tuffing.co.nz>
2018-12-09 14:05:52 +1300

    fixed a couple bugs in main.py

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ app.config.from_envvar('WEBHOOK_SETTINGS')
 
 @app.route('/')
 def index():
-    return 'Tuffing web hooks'
+	return 'Tuffing web hooks'
 
 @app.route('/gitpush/<repo>', methods=['GET', 'POST'])
 def gitpush(repo):
-        valid = False
-        
-        for ranges in app.config['WHITELIST']:
-            if IPv4Address(request.environ.get('HTTP_X_REAL_IP', request.remote_addr)) in IPv4Network(ranges):
-                valid = True
+	valid = False
+	
+	for ranges in app.config['WHITELIST']:
+		if IPv4Address(request.environ.get('HTTP_X_REAL_IP', request.remote_addr)) in IPv4Network(ranges):
+			valid = True
 
-        if not valid:
+	if not valid:
 		return 'invalid ip'
 
 	if repo not in app.config['REPOS']:
