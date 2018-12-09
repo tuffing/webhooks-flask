85dc51da1b364a8e8a30f4afb32fb55adec2988b
logan on pi <logan.orr@tuffing.co.nz>
2018-12-09 02:50:35 +0000

    updated update script with proper paths and clean up

--- updatescript.py
+++ updatescript.py
@@ -5,14 +5,9 @@ repos = inputFile.read().strip().split('\n')
 inputFile.truncate(0)
 inputFile.close()
 
-
-github = GITPATH = '/home/logan/projects/%s'
-GITURL = 'git@github.com:tuffing/%s.git'
-
-
 for repo in repos: 
 	#update local repo from it's source
-	g = git.cmd.Git('/home/logan/projects/%s' % repo)
+	g = git.cmd.Git('/home/pi/gitprojects/%s' % repo)
 	g.pull()
 
 	g.push('git@github.com:tuffing/%s.git' % repo)
